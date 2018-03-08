cb66e725f55971c638ef814d8871ebd920f3c044
Amarjeet Singh <33446077+amarjeet045@users.noreply.github.com>
2018-03-08 11:41:54 -0800

    Create jsquiz.js

---
+++ jsquiz.js
@@ -0,0 +1,10 @@
+(function() {
+  var questions = [{
+    question: "which is first indian cricket tournament?",
+    choices: ['none of these','Bombay Triangular','pepsi cup','bombay series'],
+    correctAnswer: 1
+  }, {
+    question: "indian played first test match against?",
+    choices: ['Australia',' england ','south Africa','pakistan'],
+    correctAnswer: 1
+  },
